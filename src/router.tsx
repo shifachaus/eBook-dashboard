c3dce8b9fa597cb53d92696c99c13a24d29fe304
shifachaus <chausshifa804@gmail.com>
2024-05-01 15:09:22 +0530

    add register page

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "./pages/Login";
 import HomePage from "./pages/Home";
+import RegisterPage from "./pages/Register";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
   {
     path: "/login",
     element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
   },
 ]);
 
