5988415da8841109ea40ba04e4ac5487c6760f30
shifachaus <chausshifa804@gmail.com>
2024-05-03 13:17:50 +0530

    persiste token on register

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -9,23 +9,26 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { register } from "@/http/api";
+import useTokenStore from "@/store";
 import { useMutation } from "@tanstack/react-query";
 import { LoaderCircle } from "lucide-react";
 import { useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
+  const navigate = useNavigate();
+  const setToken = useTokenStore((state) => state.setToken);
+
   const nameRef = useRef<HTMLInputElement>(null);
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
 
-  const navigate = useNavigate();
-
   // Mutations
   const mutation = useMutation({
     mutationFn: register,
-    onSuccess: () => {
+    onSuccess: (response) => {
       console.log("Register successful");
+      setToken(response.data.accessToken);
       navigate("/dashboard/home");
     },
   });
