43aac597a7b8e622abb106ef94bddcce00a65f87
shifachaus <chausshifa804@gmail.com>
2024-05-04 17:01:05 +0530

    add react hook form

--- src/pages/CreateBook.tsx
+++ src/pages/CreateBook.tsx
@@ -14,79 +14,138 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import { useForm } from "react-hook-form";
 
 const CreateBook = () => {
+  const form = useForm();
+
   return (
     <section>
-      <div className="flex justify-between items-center">
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem>
-              <BreadcrumbLink href="/dashboard/home">Home</BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbLink href="/dashboard/books">Books</BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbPage>Create</BreadcrumbPage>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
+      <Form {...form}>
+        <form className="space-y-8">
+          <div className="flex justify-between items-center">
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem>
+                  <BreadcrumbLink href="/dashboard/home">Home</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                  <BreadcrumbLink href="/dashboard/books">Books</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Create</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
 
-        <div className="flex items-center gap-2">
-          <Button variant={"outline"} size={"sm"}>
-            <span className="ml-2">Cancel</span>
-          </Button>
+            <div className="flex items-center gap-2">
+              <Button variant={"outline"} size={"sm"}>
+                <span className="ml-2">Cancel</span>
+              </Button>
 
-          <Button size={"sm"}>
-            <span className="ml-2">Submit</span>
-          </Button>
-        </div>
-      </div>
-
-      <Card className="mt-6">
-        <CardHeader>
-          <CardTitle>Create a new books</CardTitle>
-          <CardDescription>
-            Fill out the form below to create a new book
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="grid gap-6">
-              <div className="grid gap-3">
-                <Label htmlFor="name">Name</Label>
-                <Input id="name" type="text" className="w-full" required />
-              </div>
-
-              <div className="grid gap-3">
-                <Label htmlFor="genre">Genre</Label>
-                <Input id="genre" type="text" className="w-full" />
-              </div>
-
-              <div className="grid gap-3">
-                <Label htmlFor="description">Description</Label>
-                <Textarea id="description" className="min-h-32" />
-              </div>
-
-              <div className="grid gap-3">
-                <Label htmlFor="coverImage">Cover Image</Label>
-                <Input type="file" id="coverImage" />
-              </div>
-
-              <div className="grid gap-3">
-                <Label htmlFor="bookFile">Book PDF</Label>
-                <Input type="file" id="bookFile" />
-              </div>
+              <Button type="submit" size={"sm"}>
+                <span className="ml-2">Submit</span>
+              </Button>
             </div>
-          </form>
-        </CardContent>
-      </Card>
+          </div>
+
+          <Card className="mt-6">
+            <CardHeader>
+              <CardTitle>Create a new books</CardTitle>
+              <CardDescription>
+                Fill out the form below to create a new book
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="grid gap-6">
+                <FormField
+                  control={form.control}
+                  name="title"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Book Title</FormLabel>
+                      <FormControl>
+                        <Input type="text" className="w-full" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="genre"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Genre</FormLabel>
+                      <FormControl>
+                        <Input type="text" className="w-full" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="description"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Description</FormLabel>
+                      <FormControl>
+                        <Textarea className="w-full" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="coverImage"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Cover Image</FormLabel>
+                      <FormControl>
+                        <Input type="file" className="w-full" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="bookFile"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Book PDF</FormLabel>
+                      <FormControl>
+                        <Input type="file" className="w-full" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </CardContent>
+          </Card>
+        </form>
+      </Form>
     </section>
   );
 };
