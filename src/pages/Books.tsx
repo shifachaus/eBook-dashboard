eb89180abefe8eec88ee328f646a89b23f6c4b80
shifachaus <chausshifa804@gmail.com>
2024-05-04 16:18:07 +0530

    add loading spinner, and error message

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -34,7 +34,7 @@ import {
 import { getBooks } from "@/http/api";
 import { Book } from "@/types";
 import { useQuery } from "@tanstack/react-query";
-import { MoreHorizontal } from "lucide-react";
+import { LoaderCircle, MoreHorizontal } from "lucide-react";
 
 const BooksPage = () => {
   const { data, isLoading, isError } = useQuery({
@@ -42,6 +42,8 @@ const BooksPage = () => {
     queryFn: getBooks,
     staleTime: 10000,
   });
+
+  console.log(data, isError);
 
   return (
     <div>
@@ -86,51 +88,63 @@ const BooksPage = () => {
             </TableHeader>
 
             <TableBody>
-              {data?.data?.map((book: Book) => {
-                return (
-                  <TableRow key={book._id}>
-                    <TableCell className="hidden sm:table-cell">
-                      <img
-                        alt={book.title}
-                        className="aspect-square rounded-md object-cover"
-                        height="64"
-                        src={book.coverImage.url}
-                        width="64"
-                      />
-                    </TableCell>
-                    <TableCell className="font-medium">{book.title}</TableCell>
-                    <TableCell>
-                      <Badge variant="outline">{book.genre}</Badge>
-                    </TableCell>
+              {isError && (
+                <p className="text-red-500 text-sm font-medium">
+                  Network Error
+                </p>
+              )}
 
-                    <TableCell className="hidden md:table-cell">
-                      {book.author.name}
-                    </TableCell>
-                    <TableCell className="hidden md:table-cell">
-                      {book.createdAt}
-                    </TableCell>
-                    <TableCell>
-                      <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                          <Button
-                            aria-haspopup="true"
-                            size="icon"
-                            variant="ghost"
-                          >
-                            <MoreHorizontal className="h-4 w-4" />
-                            <span className="sr-only">Toggle menu</span>
-                          </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent align="end">
-                          <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                          <DropdownMenuItem>Edit</DropdownMenuItem>
-                          <DropdownMenuItem>Delete</DropdownMenuItem>
-                        </DropdownMenuContent>
-                      </DropdownMenu>
-                    </TableCell>
-                  </TableRow>
-                );
-              })}
+              {isLoading ? (
+                <LoaderCircle className="animate-spin mt-3 mx-auto" />
+              ) : (
+                data?.data?.map((book: Book) => {
+                  return (
+                    <TableRow key={book._id}>
+                      <TableCell className="hidden sm:table-cell">
+                        <img
+                          alt={book.title}
+                          className="aspect-square rounded-md object-cover"
+                          height="64"
+                          src={book.coverImage.url}
+                          width="64"
+                        />
+                      </TableCell>
+                      <TableCell className="font-medium">
+                        {book.title}
+                      </TableCell>
+                      <TableCell>
+                        <Badge variant="outline">{book.genre}</Badge>
+                      </TableCell>
+
+                      <TableCell className="hidden md:table-cell">
+                        {book.author.name}
+                      </TableCell>
+                      <TableCell className="hidden md:table-cell">
+                        {book.createdAt}
+                      </TableCell>
+                      <TableCell>
+                        <DropdownMenu>
+                          <DropdownMenuTrigger asChild>
+                            <Button
+                              aria-haspopup="true"
+                              size="icon"
+                              variant="ghost"
+                            >
+                              <MoreHorizontal className="h-4 w-4" />
+                              <span className="sr-only">Toggle menu</span>
+                            </Button>
+                          </DropdownMenuTrigger>
+                          <DropdownMenuContent align="end">
+                            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                            <DropdownMenuItem>Edit</DropdownMenuItem>
+                            <DropdownMenuItem>Delete</DropdownMenuItem>
+                          </DropdownMenuContent>
+                        </DropdownMenu>
+                      </TableCell>
+                    </TableRow>
+                  );
+                })
+              )}
             </TableBody>
           </Table>
         </CardContent>
